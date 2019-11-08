82ff5de6abeeda5f8e01b214b6120348984ef224
Phillipp Glanz <p.glanz@madfix.me>
2019-11-08 20:00:14 +0100

    Add dependency repo Improve code Remove type token static reference

--- CloudFlareModule/src/main/java/eu/cloudnetservice/cloudflare/module/database/CloudFlareDatabase.java
+++ CloudFlareModule/src/main/java/eu/cloudnetservice/cloudflare/module/database/CloudFlareDatabase.java
@@ -11,7 +11,6 @@ import de.dytanic.cloudnet.lib.utility.document.Document;
 import eu.cloudnetservice.cloudflare.core.models.CloudFlareConfig;
 import eu.cloudnetservice.cloudflare.core.models.PostResponse;
 
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -22,99 +21,100 @@ import java.util.Map;
  */
 public class CloudFlareDatabase extends DatabaseUsable {
 
-    private static final String CLOUDFLARE_CACHE = "cloudflare_cache", CLOUDFLARE_CACHE_REQ = "cloudflare_cache_dnsreq";
+	private static final String CLOUDFLARE_CACHE = "cloudflare_cache", CLOUDFLARE_CACHE_REQ = "cloudflare_cache_dnsreq";
 
-    public CloudFlareDatabase(Database database) {
-        super(database);
-        if (database.getDocument(CLOUDFLARE_CACHE) == null) {
-            database.insert(new DatabaseDocument(CLOUDFLARE_CACHE));
-        }
+	public CloudFlareDatabase(Database database) {
+		super(database);
+		if (database.getDocument(CLOUDFLARE_CACHE) == null) {
+			database.insert(new DatabaseDocument(CLOUDFLARE_CACHE));
+		}
 
-        if (database.getDocument(CLOUDFLARE_CACHE_REQ) == null) {
-            database.insert(new DatabaseDocument(CLOUDFLARE_CACHE_REQ));
-        }
-    }
+		if (database.getDocument(CLOUDFLARE_CACHE_REQ) == null) {
+			database.insert(new DatabaseDocument(CLOUDFLARE_CACHE_REQ));
+		}
+	}
 
-    public Collection<String> getAll() {
-        Collection<String> collection = database.getDocument(CLOUDFLARE_CACHE).keys();
-        collection.remove(Database.UNIQUE_NAME_KEY);
-        return collection;
-    }
+	public Collection<String> getAll() {
+		Collection<String> collection = database.getDocument(CLOUDFLARE_CACHE).keys();
+		collection.remove(Database.UNIQUE_NAME_KEY);
+		return collection;
+	}
 
-    public void putPostResponse(MultiValue<PostResponse, String> postResponse) {
-        Document document = database.getDocument(CLOUDFLARE_CACHE);
-        document.append(postResponse.getFirst().getId(), postResponse);
-        database.insert(document);
-    }
+	public void putPostResponse(MultiValue<PostResponse, String> postResponse) {
+		Document document = database.getDocument(CLOUDFLARE_CACHE);
+		document.append(postResponse.getFirst().getId(), postResponse);
+		database.insert(document);
+	}
 
-    public boolean contains(CloudFlareConfig cloudFlareConfig, String wrapper) {
-        Document document = database.getDocument(CLOUDFLARE_CACHE);
-        for (String key : document.keys()) {
-            if (!key.equalsIgnoreCase(Database.UNIQUE_NAME_KEY)) {
-                MultiValue<PostResponse, String> value = document.getObject(key,
-                                                                            new TypeToken<MultiValue<PostResponse, String>>() {}.getType());
+	public boolean contains(CloudFlareConfig cloudFlareConfig, String wrapper) {
+		Document document = database.getDocument(CLOUDFLARE_CACHE);
+		for (String key : document.keys()) {
+			if (!key.equalsIgnoreCase(Database.UNIQUE_NAME_KEY)) {
 
-                if (value != null && value.getSecond().equalsIgnoreCase(wrapper) && value.getFirst()
-                                                                                         .getCloudFlareConfig()
-                                                                                         .getDomainName()
-                                                                                         .equalsIgnoreCase(cloudFlareConfig.getDomainName())) {
-                    return true;
-                }
-            }
-        }
+				MultiValue<PostResponse, String> value = document.getObject(key,
+						TypeToken.getParameterized(MultiValue.class, PostResponse.class, String.class).getType());
 
-        return false;
-        //return document.contains(wrapper);
-    }
+				if (value != null && value.getSecond().equalsIgnoreCase(wrapper) && value.getFirst()
+						.getCloudFlareConfig()
+						.getDomainName()
+						.equalsIgnoreCase(cloudFlareConfig.getDomainName())) {
+					return true;
+				}
+			}
+		}
 
-    public void remove(String wrapper) {
-        database.getDocument(CLOUDFLARE_CACHE).remove(wrapper);
-    }
+		return false;
+		//return document.contains(wrapper);
+	}
 
-    public PostResponse getResponse(String wrapper) {
-        return database.getDocument(CLOUDFLARE_CACHE).getObject(wrapper, new TypeToken<PostResponse>() {}.getType());
-    }
+	public void remove(String wrapper) {
+		database.getDocument(CLOUDFLARE_CACHE).remove(wrapper);
+	}
 
-    public void add(PostResponse postResponse) {
-        if (postResponse == null) {
-            return;
-        }
+	public PostResponse getResponse(String wrapper) {
 
-        Document document = database.getDocument(CLOUDFLARE_CACHE_REQ);
-        if (document.contains("requests")) {
-            Map<String, PostResponse> responses = document.getObject("requests", new TypeToken<Map<String, PostResponse>>() {}.getType());
-            responses.put(postResponse.getId(), postResponse);
-            document.append("requests", responses);
-        } else {
-            document.append("requests", MapWrapper.valueableHashMap(new Return<>(postResponse.getId(), postResponse)));
-        }
+		return database.getDocument(CLOUDFLARE_CACHE).getObject(wrapper, TypeToken.get(PostResponse.class).getType());
+	}
 
-        database.insert(document);
-    }
+	public void add(PostResponse postResponse) {
+		if (postResponse == null) {
+			return;
+		}
+		Document document = database.getDocument(CLOUDFLARE_CACHE_REQ);
+		if (document.contains("requests")) {
 
-    public void remove(PostResponse postResponse) {
-        Document document = database.getDocument(CLOUDFLARE_CACHE_REQ);
-        if (document.contains("requests")) {
-            Map<String, PostResponse> responses = document.getObject("requests", new TypeToken<Map<String, PostResponse>>() {}.getType());
-            responses.remove(postResponse.getId());
-            document.append("requests", responses);
-        } else {
-            document.append("requests", new HashMap<>(0));
-        }
+			Map<String, PostResponse> responses = document.getObject("requests", TypeToken.getParameterized(Map.class, String.class, PostResponse.class).getType());
+			responses.put(postResponse.getId(), postResponse);
+			document.append("requests", responses);
+		} else {
+			document.append("requests", MapWrapper.valueableHashMap(new Return<>(postResponse.getId(), postResponse)));
+		}
+		database.insert(document);
+	}
 
-        database.insert(document);
-    }
+	public void remove(PostResponse postResponse) {
+		Document document = database.getDocument(CLOUDFLARE_CACHE_REQ);
+		if (document.contains("requests")) {
+			Map<String, PostResponse> responses = document.getObject("requests", TypeToken.getParameterized(Map.class,String.class,PostResponse.class).getType());
+			responses.remove(postResponse.getId());
+			document.append("requests", responses);
+		} else {
+			document.append("requests", new HashMap<>(0));
+		}
 
-    public Map<String, MultiValue<PostResponse, String>> getAndRemove() {
-        Document document = database.getDocument(CLOUDFLARE_CACHE_REQ);
-        if (document.contains("requests")) {
-            Map<String, MultiValue<PostResponse, String>> responses = document.getObject("requests",
-                                                                                         new TypeToken<Map<String, MultiValue<PostResponse, String>>>() {}
-                                                                                             .getType());
-            document.append("requests", Collections.EMPTY_MAP);
-            database.insert(document);
-            return responses;
-        }
-        return Collections.EMPTY_MAP;
-    }
+		database.insert(document);
+	}
+
+	public Map<String, MultiValue<PostResponse, String>> getAndRemove() {
+		Document document = database.getDocument(CLOUDFLARE_CACHE_REQ);
+		if (document.contains("requests")) {
+
+			Map<String, MultiValue<PostResponse, String>> responses = document.getObject("requests",
+                    TypeToken.getParameterized(Map.class,String.class, TypeToken.getParameterized(MultiValue.class, PostResponse.class, String.class).getType()).getType());
+			document.append("requests", Collections.EMPTY_MAP);
+			database.insert(document);
+			return responses;
+		}
+		return Collections.EMPTY_MAP;
+	}
 }
